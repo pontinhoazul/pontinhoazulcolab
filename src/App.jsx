a8c62a2b110de058229cf5fd020aaf58e3fcbc93
pontinhoazul <sarapontinhoazul@gmail.com>
2026-06-11 18:49:54 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import ListaEspera from './components/pages/ListaEspera'
-
-// Estilos básicos apenas para o site não ficar totalmente desalinhado
 import './index.css' 
 
 export default function App() {
   return (
     <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-      <h1 style={{ color: '#0070f3' }}>Painel Clínica Pontinho Azul</h1>
+      <h1 style={{ color: '#0070f3', marginBottom: '20px' }}>Painel Clínica Pontinho Azul</h1>
       <ListaEspera />
     </div>
   )
